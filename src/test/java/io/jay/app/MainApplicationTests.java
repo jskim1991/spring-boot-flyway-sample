d76155215f65f5e0b13ff6a46ab9328511dc35d4
Jay Kim <tensaijskim1991@gmail.com>
2023-06-07 13:58:10 +0900

    fix typo

--- src/test/java/io/jay/app/MainApplicationTests.java
+++ src/test/java/io/jay/app/MainApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"spring.flyway.clean-disabled=false"})
-class MainApplicationTests extends TestContainerInitializer {
+class MainApplicationTests extends PostgresTestContainerInitializer {
 
     @BeforeEach
     void setup(@Autowired Flyway flyway) {
